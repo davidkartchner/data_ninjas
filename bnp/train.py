80aa1ce946b92680a6f1a1a24d5deed7f2a6bfef
Benjamin Pachev <benjaminpachev@gmail.com>
2016-03-24 14:32:58 -0600

    Started writing training script.

---
+++ bnp/train.py
@@ -0,0 +1,13 @@
+from sklearn.Ensemble import RandomForestClassifier
+import pandas as pd
+
+def remove_nans(arr):
+ nan_mask = np.isnan(arr)
+ med = np.median(arr[np.logical_not(nan_mask)])
+ arr[nan_mask] = med
+
+def cleanup_data():
+
+#rf = RandomForestClassifier(n_estimators = 100)
+#rf.fit(train, target)
+
