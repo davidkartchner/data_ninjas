5b27ddbf424b10bdec581cf8f298af96b5723ac8
davidkartchner <davidspencerkartchner@byu765045wks.app.byu.edu>
2016-03-24 14:30:54 -0600

    Add data cleaning script

---
+++ bnp/competition_cleaning.py
@@ -0,0 +1,16 @@
+#David Kartchner
+#March 24, 2016
+
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from matplotlib import pyplot as plt
+
+data = pd.read_csv('train.csv')
+
+pd.set_option('expand_frame_repr', True)
+pd.set_option('max_columns',200)
+data[:10]
+for i in data.select_dtypes(include=['object'])[:0]:
+    data[i] = data[i].astype('category')
